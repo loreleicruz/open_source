88455a60560b85888833122ca289654839fc3477
ted <tedward603@gmail.com>
2015-04-20 15:00:48 +0100

    change in main2

--- main2.c
+++ main2.c
@@ -1,5 +1,7 @@
 #include <my_global.h>
 #include <mysql.h>
+#include <string.h>
+#include <stdio.h>
 
 void finish_with_error(MYSQL *con)
 {
@@ -14,30 +16,30 @@ int main(int argc, char **argv)
   
   if (con == NULL)
   {
-      fprintf(stderr, "mysql_init() failed\n");
-      exit(1);
+    fprintf(stderr, "mysql_init() failed\n");
+    exit(1);
   }  
   
   if (mysql_real_connect(con,"localhost","root","toor","process_control",0,NULL,0)==NULL)
   {
-      finish_with_error(con);
+    finish_with_error(con);
   }    
   
   if (mysql_query(con, "SELECT timestamp FROM mixer_temp")) 
   {
-      finish_with_error(con);
+    finish_with_error(con);
   }
   MYSQL_RES *result = mysql_store_result(con);
 
   if (mysql_query(con, "SELECT temperature FROM mixer_temp")) 
   {
-      finish_with_error(con);
+    finish_with_error(con);
   }
   MYSQL_RES *result2 = mysql_store_result(con);
 
   if (result == NULL || result2 == NULL) 
   {
-      finish_with_error(con);
+    finish_with_error(con);
   }
 
 
@@ -45,15 +47,32 @@ int main(int argc, char **argv)
 
   MYSQL_ROW row;
   MYSQL_ROW row2;
-  while ((row = mysql_fetch_row(result))&&(row = mysql_fetch_row(result2))) 
-  { 
-      for(int i = 0; i < num_fields; i++) 
-      { 
-          printf("%s,%s", row[i] ? row[i] : "NULL",row2[i] ? row2[i] : "NULL");
-      } 
-          printf("\n"); 
-  }
+
+  char str[100];
+
+  printf("\n Enter the filename :");
+
+ scanf("%s", str);
+
+  FILE *fp;
+  char *filename = str;
+  filename=strcat(filename,".csv");
   
+
+fp=fopen(filename,"w+");
+ int j=0;
+  while ((row = mysql_fetch_row(result))&&(row2 = mysql_fetch_row(result2))) 
+  { 
+    
+    for(int i = 0; i < num_fields; i++) 
+    { 
+
+      printf("%d,%s \n",j,row2[i] ? row2[i] : "NULL");
+      fprintf(fp,"%d,%s \n",j,row2[i] ? row2[i] : "NULL");
+    }  
+    j++;
+  }
+   fclose(fp);
   mysql_free_result(result);
   mysql_close(con);
   
